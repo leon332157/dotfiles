d9d95930cb0e90c956d0d4d1d71113985751e3fd
Leon Ge <leon332157@gmail.com>
2024-03-03 02:18:56 -0500

    do not clone if dir already exist

--- run_onchange_install_tpm.sh
+++ run_onchange_install_tpm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
